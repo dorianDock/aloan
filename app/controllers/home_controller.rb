1e41979e59bc704b26dfd5b4d846dfb4faee30ae
Dorian <gaillotdorian@gmail.com>
2016-12-13 16:37:04 +0100

    making views for signing up and signing in really better, adding logo there

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,14 @@
 class HomeController < ApplicationController
 
+  # skip_before_action :authenticate_user!, only: [:index]
+
   def index
 
   end
 
-  
+  def secured
+
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  # home
   root 'home#index'
+  get 'home/secured'
+
 end
