0be7954c01f6e8f6459d2090ecce8b80b8a88559
Dorian <gaillotdorian@gmail.com>
2017-01-08 15:02:07 +0100

    creating loan_duration and loan_duration_in_months for a loan

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -18,6 +18,7 @@
 
 class Loan < ApplicationRecord
 
+  DAYS_IN_A_MONTH = 30
   belongs_to :borrower
 
   validates :start_date, presence: { message: I18n.t('loan.not_blank')}
@@ -33,16 +34,33 @@ class Loan < ApplicationRecord
 
   def beginning_difference_days
     # we want days
-    (self.start_date.to_i-Time.now.to_i)/(3600*24)
+    (self.start_date.to_i-Time.now.to_i)/(3600*24)+1
   end
 
   def end_difference_days
     # we want days
-    (self.contractual_end_date.to_i-Time.now.to_i)/(3600*24)
+    (self.contractual_end_date.to_i-Time.now.to_i)/(3600*24)+1
   end
 
   def interest
     self.amount*(self.rate/100)
+  end
+
+  def loan_duration
+    (self.contractual_end_date.to_i-self.start_date.to_i)/(3600*24)+1
+  end
+
+  # takes a number of days into a number in months
+  def loan_duration_in_months
+    integer_part = self.loan_duration/DAYS_IN_A_MONTH
+    rest = self.loan_duration%DAYS_IN_A_MONTH
+    total = integer_part
+    if rest > 20
+      total += 1
+    elsif rest > 10 && rest <=20
+      total += 0.5
+    end
+    total
   end
 
 end
