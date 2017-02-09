0eaa462a086f6df04548ced825a4dce51b7189fd
Dorian <gaillotdorian@gmail.com>
2017-02-09 18:10:57 +0100

    internationalizing first stat

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -2,7 +2,7 @@ class StatisticsController < ApplicationController
 
   def index
     @stats = Array.new
-    @a_stat = Statistics.new('Argent prêté', 'Argent actuellement prêté durant cette vague de prêts')
+    @a_stat = Statistics.new(t('stats.lent_money_title'), t('stats.lent_money_description'))
     @a_stat.calculate_money_outside
     @stats << @a_stat
   end
