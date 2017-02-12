d73503d78608d93f185104b82f8eafcd69c182fe
Dorian <gaillotdorian@gmail.com>
2017-02-12 14:31:47 +0100

    tests on statistics controller

--- spec/controllers/statistics_controller_spec.rb
+++ spec/controllers/statistics_controller_spec.rb
@@ -2,4 +2,26 @@ require 'rails_helper'
 
 RSpec.describe StatisticsController, type: :controller do
 
+  include Devise::Test::ControllerHelpers
+
+
+  describe 'Anonymous must be authenticated to see one of the protected pages of statistics' do
+    it 'statistics#index asks for authenticating' do
+      get :index
+      expect(response).to have_http_status(:redirect)
+      # redirected to the sign in page
+    end
+  end
+
+  describe 'Actions working for loans >' do
+    before(:each) do
+      @user= FactoryGirl.create(:user)
+      sign_in @user
+    end
+    it 'statistics#index is reachable' do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
+
 end
