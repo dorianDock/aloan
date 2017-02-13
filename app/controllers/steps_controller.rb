e171238fe9ab83bbdf69f2ebb04ccdc44802eaed
Dorian <gaillotdorian@gmail.com>
2017-02-13 12:29:41 +0100

    creating specific views for steps with loans/loan_t + new and create action for steps

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -1,4 +1,46 @@
 class StepsController < ApplicationController
+
+
   def new
+    @new_step = Step.new
+    my_html=render_to_string('steps/_new_step', :formats => [:html], :layout => false, :locals => {:step => @new_step})
+    respond_to do |format|
+      format.json {
+        render json: {:partial_view => my_html }
+      }
+    end
   end
+
+  # we have one create action for two cases: whether it is a loan or a loan template
+  def create
+    permitted_params = permitted_params(params[:step])
+    @new_step = Step.new(permitted_params)
+
+    if @new_step.valid?
+      @new_step.save!
+      loan_id = permitted_params[:loan_id]
+      loan_template_id = permitted_params[:loan_template_id]
+      my_html = ''
+      if loan_id
+        my_html=render_to_string('steps/_step_for_loan', :formats => [:html], :layout => false, :locals => {:step => @new_step})
+      end
+      if loan_template_id
+        my_html=render_to_string('steps/_step_for_loan_template', :formats => [:html], :layout => false, :locals => {:step => @new_step})
+      end
+      respond_to do |format|
+        format.json {
+          render json: {:partial_view => my_html, :step_id => @new_step.id }
+        }
+      end
+    end
+  end
+
+
+
+  protected
+
+  def permitted_params(params)
+    params.permit(:loan_id, :step_type_id, :expected_date, :date_done, :amount, :loan_template_id)
+  end
+
 end
