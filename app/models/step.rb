9525b2316d39b94465ba7bab4e87d3b311a0d497
Dorian <gaillotdorian@gmail.com>
2017-02-14 12:10:48 +0100

    2 validation rules for days and months after the previous milestone for a step: they should not be used at the same time and at least one is mandatory

--- app/models/step.rb
+++ app/models/step.rb
@@ -2,16 +2,18 @@
 #
 # Table name: steps
 #
-#  id               :integer          not null, primary key
-#  loan_id          :integer
-#  step_type_id     :integer
-#  expected_date    :datetime
-#  date_done        :datetime
-#  is_done          :boolean
-#  amount           :float
-#  created_at       :datetime         not null
-#  updated_at       :datetime         not null
-#  loan_template_id :integer
+#  id                              :integer          not null, primary key
+#  loan_id                         :integer
+#  step_type_id                    :integer
+#  expected_date                   :datetime
+#  date_done                       :datetime
+#  is_done                         :boolean
+#  amount                          :float
+#  created_at                      :datetime         not null
+#  updated_at                      :datetime         not null
+#  loan_template_id                :integer
+#  days_after_previous_milestone   :integer
+#  months_after_previous_milestone :integer
 #
 
 class Step < ApplicationRecord
@@ -26,11 +28,26 @@ class Step < ApplicationRecord
 
   validate :loan_or_loan_template
   validate :not_loan_and_loan_template
+  validate :days_or_months_after_previous
+  validate :not_days_and_months_after_previous
 
 
 
 
   private
+
+  def days_or_months_after_previous
+    if (days_after_previous_milestone.nil? || days_after_previous_milestone == '') && (months_after_previous_milestone.nil? || months_after_previous_milestone == '')
+      errors.add(:days_after_previous_milestone, I18n.t('step.at_least_days_or_months_after_prev'))
+    end
+  end
+
+  def not_days_and_months_after_previous
+    if (days_after_previous_milestone.nil? || days_after_previous_milestone == '') && (months_after_previous_milestone.nil? || months_after_previous_milestone == '')
+      errors.add(:loan_id, I18n.t('step.not_days_and_months_after_prev'))
+    end
+  end
+
 
   def loan_or_loan_template
     if (loan_id.nil? || loan_id == '') && (loan_template_id.nil? || loan_template_id == '')
--- spec/factories/steps.rb
+++ spec/factories/steps.rb
@@ -2,16 +2,18 @@
 #
 # Table name: steps
 #
-#  id               :integer          not null, primary key
-#  loan_id          :integer
-#  step_type_id     :integer
-#  expected_date    :datetime
-#  date_done        :datetime
-#  is_done          :boolean
-#  amount           :float
-#  created_at       :datetime         not null
-#  updated_at       :datetime         not null
-#  loan_template_id :integer
+#  id                              :integer          not null, primary key
+#  loan_id                         :integer
+#  step_type_id                    :integer
+#  expected_date                   :datetime
+#  date_done                       :datetime
+#  is_done                         :boolean
+#  amount                          :float
+#  created_at                      :datetime         not null
+#  updated_at                      :datetime         not null
+#  loan_template_id                :integer
+#  days_after_previous_milestone   :integer
+#  months_after_previous_milestone :integer
 #
 
 FactoryGirl.define do
