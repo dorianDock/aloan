338f6495dbe0ecd1fd040150b9b74411747a9345
Dorian <gaillotdorian@gmail.com>
2017-02-18 14:28:48 +0100

    tests of helper method step_display_time

--- app/helpers/steps_helper.rb
+++ app/helpers/steps_helper.rb
@@ -4,7 +4,7 @@ module StepsHelper
     if duration_in_months == 0
       t('step.initial')
     else
-      t('step.time1')+duration_in_months+t('month', :count => duration_in_months)
+      t('step.time1')+duration_in_months.to_s+' '+t('month', :count => duration_in_months)
     end
   end
 
--- spec/helpers/steps_helper_spec.rb
+++ spec/helpers/steps_helper_spec.rb
@@ -11,5 +11,23 @@ require 'rails_helper'
 #   end
 # end
 RSpec.describe StepsHelper, type: :helper do
+  describe 'step_display_time(duration_in_months)' do
+
+    it 'returns initial step when the duration is 0 month' do
+      result = step_display_time(0)
+      expect(result).to eq(t('step.initial'))
+    end
+
+    it 'is correct when the duration is 1 month' do
+      result = step_display_time(1)
+      expect(result).to eq('After 1 month')
+    end
+
+    it 'is correct when the duration is 2 months' do
+      result = step_display_time(2)
+      expect(result).to eq('After 2 months')
+    end
+
+  end
 
 end
