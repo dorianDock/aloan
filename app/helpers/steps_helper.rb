c82b0c19c8baf32a9f58d9bd4e2385bec0791044
Dorian <gaillotdorian@gmail.com>
2017-02-18 14:18:22 +0100

    creation of duration in months helper

--- app/helpers/steps_helper.rb
+++ app/helpers/steps_helper.rb
@@ -1,2 +1,12 @@
 module StepsHelper
+
+  def step_display_time(duration_in_months)
+    if duration_in_months == 0
+      t('step.initial')
+    else
+      t('step.time1')+duration_in_months+t('month', :count => duration_in_months)
+    end
+  end
+
+
 end
